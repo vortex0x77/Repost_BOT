52aaa4b84451b487413d1050ca3aedd12f0c5e8b
vortex0x77 <4lexa9der@gmail.com>
2025-05-05 18:20:18 +0300

    Переписанный модуль

--- states.py
+++ states.py
@@ -5,5 +5,5 @@ class QuestionStates(StatesGroup):
     waiting_for_description = State()   # Ожидание описания вопроса
 
 class AnswerStates(StatesGroup):
-    waiting_for_contact = State()       # Ожидание контакта для онлайн-ответа
-    waiting_for_meeting_time = State()  # Ожидание времени для офлайн-ответа
+    waiting_for_contact = State()       # Ожидание контакта для онлайн ответа
+    waiting_for_meeting_time = State()  # Ожидание времени для офлайн ответа
