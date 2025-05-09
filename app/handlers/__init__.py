ac2d66b5002be6c681ac0ed5162e228cc6fd688a
vortex0x77 <4lexa9der@gmail.com>
2025-05-09 12:36:04 +0300

    Немного исправил код

--- app/handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Router
-
-from .admin_handlers import admin_router
-from .user_handlers import user_router
-
-main_router = Router()
-main_router.include_routers(user_router, admin_router)
